2ad12fd1dbddf76aebfec84981aafec7f14f48d6
Прушак Евгений Владимирович <D:\poshta>
2020-04-08 16:02:00 +0300

    feat: add function assignValue

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-/* Значение из текстовых  и range инпутов */
-const totalCost = document.getElementById('total-cost'),
-    totalCostRange = document.getElementById('total-cost-range'),
-    anInitialFee = document.getElementById('an-initial-fee'),
+/* Значение из текстовых   инпутов */
+const totalCost = document.getElementById('total-cost'),  
+    anInitialFee = document.getElementById('an-initial-fee'), 
+    creditTerm = document.getElementById('credit-term');
+  
+/* Значение из range инпутов */
+
+const totalCostRange = document.getElementById('total-cost-range'),
     anInitialFeeRange = document.getElementById('an-initial-fee-range'),
-    creditTerm = document.getElementById('credit-term'),
     creditTermRange = document.getElementById('credit-term-range');
- 
+
+    /* Итоговые значения */
+ const totalAmountOfCredit = document.getElementById('amount-of-credit'),
+ totalMountlyPayment = document.getElementById('mountly-payment'),
+ totalRecommendedIncome = document.getElementById('recommended-income');
+
+    /* Все range  */
+ const inputsRange = document.querySelectorAll('.input-range');
+
+    /* Все button %  */
+const bankBtns = document.querySelectorAll('.bank');
+
+const assignValue = () => {
+    totalCost.value = totalCostRange.value;
+    anInitialFee.value = anInitialFeeRange.value;
+    creditTerm.value = creditTermRange.value;
+}
+
+assignValue();
