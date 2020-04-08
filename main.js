69a93fbc35897973eb1523013f689e58de4f366e
Прушак Евгений Владимирович <D:\poshta>
2020-04-08 15:26:21 +0300

    feat: add  6 const

---
+++ main.js
@@ -0,0 +1,8 @@
+/* Значение из текстовых  и range инпутов */
+const totalCost = document.getElementById('total-cost'),
+    totalCostRange = document.getElementById('total-cost-range'),
+    anInitialFee = document.getElementById('an-initial-fee'),
+    anInitialFeeRange = document.getElementById('an-initial-fee-range'),
+    creditTerm = document.getElementById('credit-term'),
+    creditTermRange = document.getElementById('credit-term-range');
+ 
